6aefb004334a92c8f12a180a2918d32c0b5521fd
AnuradhaPolepalli <anupolepalli05@gmail.com>
2025-10-16 11:17:45 +0530

    add files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Hero } from "./hero/hero";
+import { Landing } from "./landing/landing";
+import { Counter } from "./counter/counter";
+import { Testimonial } from "./testimonial/testimonial";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Hero, Landing, Counter, Testimonial, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
